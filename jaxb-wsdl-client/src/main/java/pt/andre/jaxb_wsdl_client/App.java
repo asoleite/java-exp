a2ecb3e8e602b2de060ace57d003148c1ab44475
andre <andre@mbp-de-andre>
2017-10-03 20:22:23 +0100

    Add project jaxb-wsdl-client

---
+++ jaxb-wsdl-client/src/main/java/pt/andre/jaxb_wsdl_client/App.java
@@ -0,0 +1,13 @@
+package pt.andre.jaxb_wsdl_client;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
